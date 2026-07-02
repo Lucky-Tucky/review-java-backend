3af7dcf9f0498f9d91f9c87c4400bf031b8461ee
Lakshay Chauhan <lakshaychauhan129@gmail.com>
2026-07-02 13:18:18 +0530

    First Commit

---
+++ src/main/java/review_frontend/java_edition/JavaEditionApplication.java
@@ -0,0 +1,13 @@
+package review_frontend.java_edition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEditionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/review_frontend/java_edition/JavaEditionApplicationTests.java
@@ -0,0 +1,13 @@
+package review_frontend.java_edition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEditionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
